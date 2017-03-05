72ee8e79d50d10db5dad578d668bc3e277a402f0
romennts <romennts@gmail.com>
2017-03-05 23:58:33 +0800

    updata news

--- pages/news/news.js
+++ pages/news/news.js
@@ -15,17 +15,18 @@ Page({
       'type': 'all',
       data: [],
       showMore: true,
-      remind: '上滑加载更多'
+      remind: '下滑加载更多'
     },
     loading: false,
     user_type: 'guest',
     disabledRemind: false
   },
   onLoad: function(){
-    if(app._user.is_bind){
+    if(app.openid){
       this.setData({
         user_type: !app._user.teacher ? 'student' : 'teacher'
       });
+      console.log("user.is_bind");
     }else{
       this.setData({
         user_type: 'guest',
@@ -33,106 +34,118 @@ Page({
         'active.type': 'new'
       });
     }
+    
     this.setData({
       'loading': true,
       'active.data': [],
       'active.showMore': true,
-      'active.remind': '上滑加载更多',
-      'page': 0
+      'active.remind': '下滑加载更多',
     });
-    this.getNewsList();
+    var temp_blog_data=wx.getStorageSync('blogdata');
+    var temp_lastblogid=wx.getStorageSync('lastblogid');
+    if(temp_blog_data== ''){
+      console.log("首次使用blog功能")
+      this.getNewsList();
+    }else{
+      console.log("使用过blog")
+      this.setData({
+        'active.data': temp_blog_data,
+        'active.showMore': true,
+        'active.remind': '下滑加载更多',
+        'page': temp_lastblogid
+      });
+    }
+    console.log(this.data.active.data);
   },
   //下拉更新
   onPullDownRefresh: function(){
     var _this = this;
     _this.setData({
       'loading': true,
-      'active.data': [],
       'active.showMore': true,
-      'active.remind': '上滑加载更多',
-      'page': 0
+      'active.remind': '下滑加载更多',
     });
     _this.getNewsList();
+    wx.stopPullDownRefresh();
   },
-  //上滑加载更多
+  //上滑出到底端
   onReachBottom: function(){
     var _this = this;
-    if(_this.data.active.showMore){
-      _this.getNewsList();
-    }
+    _this.setData({
+      'active.remind': '————有底线的小程序————'
+    })
   },
   //获取新闻列表
   getNewsList: function(typeId){
     var _this = this;
-    
+    var tempblog=_this.data.active.data;
+    var tempblog_size=tempblog.length
+    console.log(tempblog_size);
+    console.log("当前请求的blogid"+_this.data.page);
     typeId = typeId || _this.data.active.id;
     
-    if(!_this.data.page){
-      _this.setData({
-        'active.data': _this.data.list[typeId].storage
-      });
-    }
     _this.setData({
       'active.remind': '正在加载中'
     });
     wx.showNavigationBarLoading();
     wx.request({
-      url: app._server  + _this.data.list[typeId].url,
-      data: {
-        blogid: _this.data.page,
-        openid: app.openid
-      },
+      url: app._server  + _this.data.list[typeId].url + '?blogid='+_this.data.page+'&openid='+app.openid,
+      method: 'GET',
       success: function(res){
         console.log(res)
-        if(res.data && res.data.status === 20010){
-          var blogdata=res.data.data;
-          var size=res.data.size
-          var lastblogid=res.data.lastblogid;
-            _this.setData({
-        'loading': true,
-        'active.data': blogdata,
-        'active.showMore': true,
-        'active.remind': '上滑加载更多',
-        'page': lastblogid
-      });
-        wx.showToast({
-        title: '更新了'+size+'条数据',
-        icon: 'success',
-        duration: 2000
-      });
-        }else{
-          if(res.data.status === 40705){
-            wx.showToast({
+        var blogdata=res.data.data;
+        var size=res.data.size
+        if(size==0){
+          wx.showToast({
         title: '亲，没有更多数据了噢~~',
         icon: 'success',
         duration: 1500
       });
-          _this.setData({
-            'active.remind': '已经是最新数据啦'
-          });
-          }else{
-          app.showErrorModal('接口暂时还没有开放');
-          _this.setData({
-            'active.remind': '待开放的功能'
-          });
+        }else{
+          var j=0;
+          for(var i=size;i<size+tempblog_size;i++){
+            blogdata[i]=tempblog[j];
+            j++;
           }
+          wx.showToast({
+        title: '更新了'+size+'条数据',
+        icon: 'success',
+        duration: 2000
+      });
+      app.saveCache('blogdata',blogdata);
+      _this.setData({
+          'active.remind': '————有底线的小程序————',
+          'active.data': blogdata,
+        });
         }
+        
+        var lastblogid=res.data.lastblogid;
+        if(lastblogid>0){
+          app.saveCache('lastblogid',lastblogid);
+          _this.setData({
+            'page': lastblogid
+          });
+        }
+        
+        _this.setData({
+        'active.showMore': true
+      });
+
       },
-      fail: function(res){
+      fail: function() {
         app.showErrorModal(res.errMsg);
         _this.setData({
           'active.remind': '网络错误'
         });
       },
-      complete: function(){
+      complete: function() {
         wx.hideNavigationBarLoading();
-        wx.stopPullDownRefresh();
-        app.saveCache('blogdata',_this.data.active.data);
         _this.setData({
           loading: false
         });
       }
-    });
+    })
+    
   },
   //获取焦点
   changeFilter: function(e){
@@ -142,7 +155,7 @@ Page({
         'type': e.target.id,
         data: [],
         showMore: true,
-        remind: '上滑加载更多'
+        remind: '下滑加载更多'
       },
       'page': 0
     });
